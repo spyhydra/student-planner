3e889ada8975bed546adf05125172eaee4c610e9
Chetan Patil <49969478+spyhydra@users.noreply.github.com>
2024-04-06 12:36:16 +0530

    login changes done

---
+++ time_planner/views/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from ..models import User
+
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput())
---
+++ time_planner/views/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import SignUpForm, LoginForm
+from django.contrib import messages
+from ..models import User
+from django.http import HttpResponse
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            print("data entered")
+            form.save()
+            messages.success(request,'Your account has been created')
+            return redirect('login')  # Redirect to login page after signup
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.filter(email=email, password=password).first()
+            if user:
+                # Login successful
+                messages.success(request,'Logged in successfully')
+                return HttpResponse("Login Successful")
+            else:
+                messages.error(request, "Invalid credentials")
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
